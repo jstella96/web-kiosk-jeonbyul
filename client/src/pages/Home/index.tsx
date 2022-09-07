e5eaf614dbcc27e4d11c39d2ec934007539be538
jstella96 <jstella0530@naver.com>
2022-09-07 12:18:29 +0900

    [HotFix] 삭제한 파일 import하는 부분 삭제  (#79)
    * refactor: 🔨Modal 타입스크립트로 마이그레이션 및  styled component 적용
    
    * refactor: 🔨타입스크립트로 마이그레이션 및  styled component 적용
    
    * refactor: 🔨 CashModal타입스크립트로 마이그레이션 및  styled component 적용
    
    * refactor: 🔨 orderItem 타입스크립트로 마이그레이션 및  styled component 적용
    
    * refactor: 🔨 Container -> Layout 변수명 변경
    
    * remove: :fire: scss 파일 제거
    
    * css: :art: 사용하지 않는 scss파일 삭제 및 home화면 css변경

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
+import COLORS from 'constants/color';
 import { useOrderInfo } from 'context/orderInfoContext';
 import { usePage } from 'context/pageContext';
 import { clearOrderInfo } from 'hooks/orderInfoState';
 import React, { useEffect } from 'react';
-
-import './index.scss';
+import styled from 'styled-components';
 
 const Home = () => {
   const { movePage } = usePage();
@@ -14,13 +14,47 @@ const Home = () => {
   }, [orderInfoDispatch]);
 
   return (
-    <div className="home">
-      <h2>Le Cordon Bleu</h2>
-      <div className="home-button">
+    <HomeLayout>
+      <h2>
+        Le <br />
+        Cordon <br />
+        Bleu
+      </h2>
+      <ButtonBox>
         <button onClick={() => movePage('main')}>매장이용</button>
         <button onClick={() => movePage('main')}>포장</button>
-      </div>
-    </div>
+      </ButtonBox>
+    </HomeLayout>
   );
 };
 export default Home;
+
+const HomeLayout = styled.div`
+  background-color: ${COLORS.primary};
+  padding: 3rem;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: space-between;
+  font-size: calc(20px + 2vmin);
+  color: ${COLORS.white};
+  h2 {
+    font-size: 4.2rem;
+    font-weight: 700;
+    letter-spacing: 0.25rem;
+  }
+`;
+const ButtonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  button {
+    background-color: ${COLORS.white};
+    font-size: 1.4rem;
+    margin: 1rem 0rem;
+    border-radius: 0.6rem;
+    font-weight: 700;
+    padding: 1.3rem 0rem;
+  }
+`;
