37f5623e369e851ff68255382b0d5df55e6f2cc1
jstella96 <jstella0530@naver.com>
2022-09-26 01:04:08 +0900

    [Feature] http Cache-Control header 추가(Agressive caching)

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,23 +1,26 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import HomePage from 'pages/HomePage/HomePage';
-import Main from 'pages/Main';
-import Order from 'pages/Order';
-import Payment from 'pages/Payment';
-import Receipt from 'pages/Receipt';
 import { PAGE_URL } from 'constants/pageUrl';
-import { useState } from 'react';
+import { lazy, Suspense, useState } from 'react';
+
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const Main = lazy(() => import('pages/Main'));
+const Order = lazy(() => import('pages/Order'));
+const Payment = lazy(() => import('pages/Payment'));
+const Receipt = lazy(() => import('pages/Receipt'));
 
 export const AppRoutes = () => {
   const [orderNum, setOrderNum] = useState(0);
   return (
-    <Routes>
-      <Route path={PAGE_URL.HOME} element={<HomePage />} />
-      <Route path={PAGE_URL.MAIN} element={<Main />} />
-      <Route path={PAGE_URL.ORDER} element={<Order />} />
-      <Route path={PAGE_URL.PAYMENT} element={<Payment setOrderNum={setOrderNum} />} />
-      <Route path={PAGE_URL.RECEIPT} element={<Receipt orderNum={orderNum} />} />
-      <Route path="/" element={<HomePage />} />
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path={PAGE_URL.HOME} element={<HomePage />} />
+        <Route path={PAGE_URL.MAIN} element={<Main />} />
+        <Route path={PAGE_URL.ORDER} element={<Order />} />
+        <Route path={PAGE_URL.PAYMENT} element={<Payment setOrderNum={setOrderNum} />} />
+        <Route path={PAGE_URL.RECEIPT} element={<Receipt orderNum={orderNum} />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </Suspense>
   );
 };
